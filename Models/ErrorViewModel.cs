f012665e7ff72da008654b5b112b359553cd3264
Elias Pereyra <elias13pereyra@gmail.com>
2026-08-11 21:09:16 -0300

    dotnet mvc creado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Inmobiliaria_Pereyra_Elias_Pelaez_Victor_Lagos_Maximiliano.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
