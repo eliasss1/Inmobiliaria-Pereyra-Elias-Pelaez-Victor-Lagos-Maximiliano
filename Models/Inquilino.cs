b93b298ae022b4b17e9fd40a828042e06b695eaa
Elias Pereyra <elias13pereyra@gmail.com>
2026-08-13 15:52:14 -0300

    Creacion clase Inquilino

---
+++ Models/Inquilino.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Inmobiliaria.Models;
+
+public class Inquilino
+{
+    [Key] // este valor es para indicar que es la primary Key
+    public int IdInquilino { get; set; }
+
+    [Required]
+    public string Nombre { get; set; } = "";
+
+    [Required]
+    public string Apellido { get; set; } = "";
+
+    [Required]
+    public string Dni {get; set;} = "";
+
+    public string Telefono { get; set; } = "";
+    
+    [Required, EmailAddress]
+    public string Email {get; set;} = "";
+
+    public override string ToString() => $"{Nombre} {Apellido}";
+}
