b291612e32bf6f8151c98d80007c659401f34f46
Elias Pereyra <elias13pereyra@gmail.com>
2026-08-21 20:30:09 -0300

    Repositorio Propietario e interfaz corregidos y terminados

---
+++ Models/RepositorioPropietario.cs
@@ -0,0 +1,249 @@
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using MySqlConnector;
+using Inmobiliaria.Models;
+
+
+namespace Inmobiliaria.Models
+{
+	public class RepositorioPropietario : RepositorioBase, IRepositorioPropietario
+	{
+		public RepositorioPropietario(IConfiguration configuration) : base(configuration)
+		{
+
+		}
+
+		public int Alta(Propietario p)
+		{
+			int res = -1;
+			using (MySqlConnection connection = new MySqlConnection(connectionString))
+			{
+				string sql = @"INSERT INTO Propietarios
+					(Nombre, Apellido, Dni, Telefono, Email, Clave)
+					VALUES (@nombre, @apellido, @dni, @telefono, @email, @clave);
+					SELECT LAST_INSERT_ID();";
+				using (MySqlCommand command = new MySqlCommand(sql, connection))
+				{
+					command.CommandType = CommandType.Text;
+					command.Parameters.AddWithValue("@nombre", p.Nombre);
+					command.Parameters.AddWithValue("@apellido", p.Apellido);
+					command.Parameters.AddWithValue("@dni", p.Dni);
+					command.Parameters.AddWithValue("@telefono", p.Telefono);
+					command.Parameters.AddWithValue("@email", p.Email);
+					command.Parameters.AddWithValue("@clave", p.Clave);
+					connection.Open();
+					res = Convert.ToInt32(command.ExecuteScalar());
+					p.IdPropietario = res;
+					connection.Close();
+				}
+			}
+			return res;
+		}
+		public int Baja(int id)
+		{
+			int res = -1;
+			using (MySqlConnection connection = new MySqlConnection(connectionString))
+			{
+				string sql = "DELETE FROM Propietarios WHERE IdPropietario = @id";
+				using (MySqlCommand command = new MySqlCommand(sql, connection))
+				{
+					command.CommandType = CommandType.Text;
+					command.Parameters.AddWithValue("@id", id);
+					connection.Open();
+					res = command.ExecuteNonQuery();
+					connection.Close();
+				}
+			}
+			return res;
+		}
+		public int Modificacion(Propietario p)
+		{
+			int res = -1;
+			using (MySqlConnection connection = new MySqlConnection(connectionString))
+			{
+				string sql = @"UPDATE Propietarios 
+					SET Nombre=@nombre, Apellido=@apellido, Dni=@dni, Telefono=@telefono, Email=@email, Clave=@clave
+					WHERE IdPropietario = @id";
+				using (MySqlCommand command = new MySqlCommand(sql, connection))
+				{
+					command.CommandType = CommandType.Text;
+					command.Parameters.AddWithValue("@nombre", p.Nombre);
+					command.Parameters.AddWithValue("@apellido", p.Apellido);
+					command.Parameters.AddWithValue("@dni", p.Dni);
+					command.Parameters.AddWithValue("@telefono", p.Telefono);
+					command.Parameters.AddWithValue("@email", p.Email);
+					command.Parameters.AddWithValue("@clave", p.Clave);
+					command.Parameters.AddWithValue("@id", p.IdPropietario);
+					connection.Open();
+					res = command.ExecuteNonQuery();
+					connection.Close();
+				}
+			}
+			return res;
+		}
+
+		public IList<Propietario> ObtenerLista(int paginaNro = 1, int tamPagina = 10)
+		{
+			IList<Propietario> res = new List<Propietario>();
+			using (MySqlConnection connection = new MySqlConnection(connectionString))
+			{
+				string sql = @$"
+					SELECT IdPropietario, Nombre, Apellido, Dni, Telefono, Email, Clave
+                    FROM Propietarios
+                    ORDER BY IdPropietario
+                    LIMIT {tamPagina} OFFSET {(paginaNro - 1) * tamPagina}
+				";
+				using (MySqlCommand command = new MySqlCommand(sql, connection))
+				{
+					command.CommandType = CommandType.Text;
+					connection.Open();
+					var reader = command.ExecuteReader();
+					while (reader.Read())
+					{
+						Propietario p = new Propietario
+						{
+							IdPropietario = reader.GetInt32(nameof(Propietario.IdPropietario)),
+							Nombre = reader.GetString(nameof(Propietario.Nombre)),
+							Apellido = reader.GetString(nameof(Propietario.Apellido)),
+							Dni = reader.GetString(nameof(Propietario.Dni)),
+							Telefono = reader.GetString(nameof(Propietario.Telefono)),
+							Email = reader.GetString(nameof(Propietario.Email)),
+							Clave = reader.GetString(nameof(Propietario.Clave)),
+						};
+						res.Add(p);
+					}
+					connection.Close();
+				}
+			}
+			return res;
+		}
+
+		public int ObtenerCantidad()
+		{
+			int res = 0;
+			using (MySqlConnection connection = new MySqlConnection(connectionString))
+			{
+				string sql = @$"
+					SELECT COUNT(IdPropietario)
+					FROM Propietarios
+				";
+				using (MySqlCommand command = new MySqlCommand(sql, connection))
+				{
+					command.CommandType = CommandType.Text;
+					connection.Open();
+					var reader = command.ExecuteReader();
+					if (reader.Read())
+					{
+						res = reader.GetInt32(0);
+					}
+					connection.Close();
+				}
+			}
+			return res;
+		}
+
+		public Propietario? ObtenerPorId(int id)
+		{
+			Propietario? p = null;
+			using (MySqlConnection connection = new MySqlConnection(connectionString))
+			{
+				string sql = @"SELECT IdPropietario, Nombre, Apellido, Dni, Telefono, Email, Clave 
+					FROM Propietarios
+					WHERE IdPropietario=@id";
+				using (MySqlCommand command = new MySqlCommand(sql, connection))
+				{
+					command.Parameters.Add("@id", MySqlDbType.Int32).Value = id;
+					command.CommandType = CommandType.Text;
+					connection.Open();
+					var reader = command.ExecuteReader();
+					if (reader.Read())
+					{
+						p = new Propietario
+						{
+							IdPropietario = reader.GetInt32(nameof(Propietario.IdPropietario)),
+							Nombre = reader.GetString("Nombre"),
+							Apellido = reader.GetString("Apellido"),
+							Dni = reader.GetString("Dni"),
+							Telefono = reader.GetString("Telefono"),
+							Email = reader.GetString("Email"),
+							Clave = reader.GetString("Clave"),
+						};
+					}
+					connection.Close();
+				}
+			}
+			return p;
+		}
+
+		public Propietario? ObtenerPorEmail(string email)
+		{
+			Propietario? p = null;
+			using (MySqlConnection connection = new MySqlConnection(connectionString))
+			{
+				string sql = @"SELECT IdPropietario, Nombre, Apellido, Dni, Telefono, Email, Clave 
+					FROM Propietarios
+					WHERE Email=@email";
+				using (MySqlCommand command = new MySqlCommand(sql, connection))
+				{
+					command.CommandType = CommandType.Text;
+					command.Parameters.Add("@email", MySqlDbType.VarChar).Value = email;
+					connection.Open();
+					var reader = command.ExecuteReader();
+					if (reader.Read())
+					{
+						p = new Propietario
+						{
+							IdPropietario = reader.GetInt32(nameof(Propietario.IdPropietario)),
+							Nombre = reader.GetString("Nombre"),
+							Apellido = reader.GetString("Apellido"),
+							Dni = reader.GetString("Dni"),
+							Telefono = reader.GetString("Telefono"),
+							Email = reader.GetString("Email"),
+							Clave = reader.GetString("Clave"),
+						};
+					}
+					connection.Close();
+				}
+			}
+			return p;
+		}
+
+		public IList<Propietario> BuscarPorNombre(string nombre)
+		{
+			List<Propietario> res = new List<Propietario>();
+			nombre = "%" + nombre + "%";
+			using (MySqlConnection connection = new MySqlConnection(connectionString))
+			{
+				string sql = @"SELECT IdPropietario, Nombre, Apellido, Dni, Telefono, Email, Clave 
+					FROM Propietarios
+					WHERE Nombre LIKE @nombre OR Apellido LIKE @nombre";
+				using (MySqlCommand command = new MySqlCommand(sql, connection))
+				{
+					command.Parameters.Add("@nombre", MySqlDbType.VarChar).Value = nombre;
+					command.CommandType = CommandType.Text;
+					connection.Open();
+					var reader = command.ExecuteReader();
+					while (reader.Read())
+					{
+						var p = new Propietario
+						{
+							IdPropietario = reader.GetInt32(nameof(Propietario.IdPropietario)),
+							Nombre = reader.GetString("Nombre"),
+							Apellido = reader.GetString("Apellido"),
+							Dni = reader.GetString("Dni"),
+							Telefono = reader.GetString("Telefono"),
+							Email = reader.GetString("Email"),
+							Clave = reader.GetString("Clave"),
+						};
+						res.Add(p);
+					}
+					connection.Close();
+				}
+			}
+			return res;
+		}
+
+	}
+}
